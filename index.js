21bff99dffa4276146d7737b641eb8a79c104c70
GabrielYan114 <gabriel.palermosouza@gmail.com>
2024-01-16 07:32:36 -0300

    finalizando o projeto

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let xp = 11000
 
+
+
+
+
+for (let xp = 0; xp <= 20000; xp += 900) 
+{
+  console.log(xp);{
+}
+let nome = ("Link")
 if (xp < 1000) 
-{console.log("Ferro");} 
+{console.log("O Herói de nome" +" "+ nome +" "+ "está no nível de Ferro");} 
 else if (xp >= 1001 && xp <= 2000) 
-{console.log("Bronze");}
+{console.log("O Herói de nome" +" "+ nome +" "+ "está no nível de Bronze");}
 else if (xp >= 2001 && xp <= 6000) 
-{console.log("Prata");} 
+{console.log("O Herói de nome" +" "+ nome +" "+ "está no nível de Prata");} 
 else if (xp >= 6001 && xp <= 7000)
-{console.log("Ouro")}
+{console.log("O Herói de nome" +" "+ nome +" "+ "está no nível de Ouro")}
 else if (xp >= 7001 && xp <= 8000)
-{console.log ("platina")}
+{console.log ("O Herói de nome" +" "+ nome +" "+ "está no nível de platina")}
 else if (xp >= 8001 && xp <= 9000)
-{console.log ("ascendente")}
+{console.log ("O Herói de nome" +" "+ nome +" "+ "está no nível de ascendente")}
 else if (xp >= 9001 && xp <= 10000)
-{console.log ("imortal")}
+{console.log ("O Herói de nome" +" "+ nome +" "+ "está no nível de imortal")}
 else 
-{console.log("Radiante");}
+{console.log("O Herói de nome" +" "+ nome +" "+ "está no nível de Radiante");}
+}
